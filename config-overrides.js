c2b3dde2ed0b53f313bc4ae5d82e24ce2ac0faac
Aryclenio Xavier Barros <aryclenio2010@gmail.com>
2020-11-20 11:55:52 -0300

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,16 +1,1 @@
-module.exports = {
-  webpack: function (config, env) {
-    return config;
-  },
-  jest: function (config) {
-    return config;
-  },
-  devServer: function (configFunction) {
-    return function (proxy, allowedHost) {
-      return config;
-    };
-  },
-  paths: function (paths, env) {
-    return paths;
-  },
-}
+module.exports = {}
