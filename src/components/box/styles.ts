862f37ba9c54c66c652a68caf217345857f7f56a
Albuquerquess <albuquerque.develop@gmail.com>
2021-12-31 22:36:22 -0300

    :construction: chore: create box component

---
+++ src/components/box/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 3.7em;
+  padding-top: 2em;
+  height: 100%;
+  height: 100%;
+
+  display: flex;
+  felx-direction: row;
+  align-items: flex-start;
+  justify-content: center;
+`;
