46888cdcb5eec94f772a1a5ee941cd47bdb740de
Albuquerquess <albuquerque.develop@gmail.com>
2022-01-01 22:17:18 -0300

    :construction: chore: lintin files

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import 'fast-text-encoding/text';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
