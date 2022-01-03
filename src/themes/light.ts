d4fac80fc21d766a9d5c8a02494129ef7983e315
Albuquerquess <albuquerque.develop@gmail.com>
2022-01-03 14:26:21 -0300

    :construction: add colors in the light theme

--- src/themes/light.ts
+++ src/themes/light.ts
@@ -2,7 +2,10 @@ export default {
   colors: {
     background: '#ffffff',
     text: '#000000',
+    textLight: '#ffffff',
+    textAlert: '#AC0000',
     cardBackground: '#EFEFEF',
+    cardLine: '#707070',
     buttonInput: '#D5D5D5',
     buttonRed: '#AC0000',
     buttonLight: '#00D84F',
