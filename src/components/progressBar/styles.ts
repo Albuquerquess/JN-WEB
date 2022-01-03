3fa7ffdec16a46a924aa3d29bbaea9a8b993a3bd
Albuquerquess <albuquerque.develop@gmail.com>
2022-01-03 14:27:32 -0300

    :construction: chore: change progressBar component style

--- src/components/progressBar/styles.ts
+++ src/components/progressBar/styles.ts
@@ -8,6 +8,7 @@ export const Container = styled.div`
   display: flex;
   align: center;
   justify-content: center;
+  margin-bottom: 3em;
 
   div.RSPBprogressBar {
     height: 100%;
@@ -25,12 +26,14 @@ export const Container = styled.div`
       position: absolute;
       z-index: -1;
     }
+
     div.RSPBstep > img {
       width: 4.8em;
     }
   }
 
   @media screen and (min-width: ${breakpoints.md}) {
+    margin-bottom: 8em;
     div.RSPBprogressBar > div.RSPBstep > img {
       width: 7em;
     }
