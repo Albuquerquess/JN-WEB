05330ecb52e78324ee88e57af8ade0eec888f01d
Albuquerquess <albuquerque.develop@gmail.com>
2022-03-25 12:43:40 -0300

    verify if room have furniture

--- src/pages/furnitures/index.tsx
+++ src/pages/furnitures/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useToasts } from 'react-toast-notifications';
 
 import Button from '../../components/button';
@@ -8,67 +8,51 @@ import FurnitureCard from '../../components/card/furnitureCard';
 import Loading from '../../components/loading';
 import PricePreview from '../../components/pricePreview';
 import Title from '../../components/title';
-import URLs from '../../helpers/URLs';
-import { Api } from '../../services/api';
-import { addRoom } from '../../store/actions/furnitures';
+import Requests from '../../services/api';
 import { IAppState } from '../../store/types';
 import { IVariation } from '../../types/furnitureCard';
-import { IRoom } from '../../types/redux/furnitures';
 import logger from '../../utils/logger';
 import { Container } from './styles';
 
-type furniture = {
+type IFurnitures = {
   id: string;
   furnitureName: string;
+  status: 0 | 1;
   variations: IVariation[];
 };
-interface IResponseGetFurnitures {
-  furnitures: furniture[];
-  room: IRoom;
-}
 
 const Furnitures: React.FC = () => {
-  const [furnitures, setFurnitures] = React.useState<IResponseGetFurnitures>();
+  const [furnitures, setFurnitures] = React.useState<IFurnitures[]>([]);
+  const [loading, setLoading] = React.useState<boolean>(false);
 
   const { addToast } = useToasts();
 
-  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const roomData = useSelector((state: IAppState) => state.furnitures.room);
+  const currentRoom = useSelector((state: IAppState) => state.furnitures.room);
 
   const furnituresStored = useSelector((state: IAppState) => state.furnitures);
 
   async function getFurnituresByRoom() {
-    try {
-      const response = await Api.get(URLs.getFurnitures, {
-        params: {
-          room_id: roomData.id,
-        },
+    setLoading(true);
+    const furnitures = await Requests.getFurnituresByRoomId(
+      Number(currentRoom.id),
+    );
+
+    if (furnitures.error) {
+      addToast(furnitures.messages || 'Ocorreu um erro!', {
+        appearance: 'error',
+        autoDismiss: true,
       });
-
-      if (response.status === 200) {
-        logger.log('getFurnituresByRoom', response.data);
-
-        const { id: roomId, name: roomName } = response.data.room;
-
-        setFurnitures(response.data);
-
-        dispatch(
-          addRoom({
-            id: roomId,
-            name: roomName,
-          }),
-        );
-      }
-    } catch (error) {
-      alert('Não foi possível buscar os móveis do cômodo. Tente novamente.');
     }
+
+    setFurnitures(furnitures.data);
+    setLoading(false);
   }
 
   function handleClick() {
     if (furnituresStored.selected) {
       const furnituresSelecteds = furnituresStored.selected.filter(
-        selected => selected.roomId === furnitures?.room.id,
+        selected => selected.roomId === currentRoom.id,
       );
 
       if (furnituresSelecteds.length >= 1) {
@@ -90,30 +74,41 @@ const Furnitures: React.FC = () => {
   React.useEffect(() => {
     getFurnituresByRoom();
   }, []);
+
+  React.useEffect(() => {
+    logger.log('furnitures', furnitures);
+  }, [furnitures]);
+
   return (
     <Container>
-      {furnitures ? (
+      {furnitures.length > 0 && !loading && (
         <>
           <Title
-            title={`Móveis: ${furnitures.room.name}`}
+            title={`Móveis: ${currentRoom.name}`}
             subtitle="Adicione os móveis, as medidas solicitadas de cada um e suas variações de tipo."
           />
           <div className="furniture-card-container">
-            {furnitures.furnitures.map(furniture => (
-              <FurnitureCard
-                variations={furniture.variations}
-                furniture={{
-                  furnitureName: furniture.furnitureName,
-                  id: furniture.id,
-                }}
-                room={furnitures.room}
-              />
-            ))}
+            {furnitures &&
+              furnitures.map(furniture => (
+                <FurnitureCard
+                  variations={furniture.variations}
+                  furniture={{
+                    furnitureName: furniture.furnitureName,
+                    id: furniture.id,
+                  }}
+                  room={currentRoom}
+                />
+              ))}
           </div>
           <Button handleClick={() => handleClick()} />
         </>
-      ) : (
-        <Loading />
+      )}
+      {furnitures.length === 0 && loading && <Loading />}
+      {furnitures && furnitures.length === 0 && !loading && (
+        <>
+          <h1>Não existem moveis cadastrado nesse ambiente!</h1>
+          <Link to="/ambientes">Voltar para a pagina de ambientes.</Link>
+        </>
       )}
       <PricePreview />
     </Container>
